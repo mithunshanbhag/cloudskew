78de4da19b29b5bb22f6fac45f9ba1a34d4e2a54
mithun shanbhag <mithunshanbhag@hotmail.com>
2026-05-11 00:23:14 +0530

    fix: update webAPIUrl in environment files to point to new ACA endpoint

--- src/CloudSkew.Ui/src/environments/environment.prod.ts
+++ src/CloudSkew.Ui/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   aiInstrumentationKey: 'bd71c98e-47fc-4e3b-9a38-5b01b705776c',
   cdnUrlPrefix: 'https://assets.cloudskew.com',
-  webAPIUrl: 'https://cloudskew-api.azurewebsites.net',
+  webAPIUrl: 'https://cloudskew-aca.salmonmushroom-ea5f3cf7.westeurope.azurecontainerapps.io',
   docsUrl: 'https://docs.cloudskew.com',
 };
--- src/CloudSkew.Ui/src/environments/environment.test.ts
+++ src/CloudSkew.Ui/src/environments/environment.test.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   aiInstrumentationKey: 'db116377-6a23-4d6a-945b-38af84a8db0e',
   cdnUrlPrefix: 'https://assets.cloudskew.com',
-  webAPIUrl: 'https://cloudskew-api.azurewebsites.net',
+  webAPIUrl: 'https://cloudskew-aca.salmonmushroom-ea5f3cf7.westeurope.azurecontainerapps.io',
   docsUrl: 'https://docs.cloudskew.com',
 };
